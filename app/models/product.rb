c99c9a24d25ca9c2ec7d2d1275583b2fcec9bff9
GiaThang <githang.huflit@gmail.com>
2016-05-20 15:37:53 +0700

    add validates to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+  default_scope { order('name ASC') }
+  validates :name, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :name, uniqueness: true
+  validates :image_url, format: {with: %r{\.(gif|jpg|png)\z}i , message: 'must be a URL for GIF, JPG or PNG image.'}
 end
